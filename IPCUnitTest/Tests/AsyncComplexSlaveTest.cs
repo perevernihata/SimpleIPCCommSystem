4c3b25579408306fb4b8de2cce2c8038ec1fc520
perevernihata <ivan.perevernihata@mail.com>
2013-07-14 13:28:17 -0700

    - added missed file

---
+++ IPCUnitTest/Tests/AsyncComplexSlaveTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IPCUnitTest.Tests {
+    [TestClass]
+    public class AsyncComplexSlaveTest {
+
+        [TestMethod]
+        public void DoAsyncComplexSlaveTest() {
+        }
+
+    }
+}
